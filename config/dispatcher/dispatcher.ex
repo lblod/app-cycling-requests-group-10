055a719b8edaca8bf0cbefa27584fb98dd6695cc
karel kremer <karel.kremer@redpencil.io>
2024-09-18 10:09:33 +0200

    add frontend docker and fix images

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -8,15 +8,42 @@ defmodule Dispatcher do
 
   define_layers [ :static, :resources, :services, :fall_back, :not_found ]
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule:
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-  #
+  options "/*path", _ do
+    conn
+    |> Plug.Conn.put_resp_header("access-control-allow-headers", "content-type,accept")
+    |> Plug.Conn.put_resp_header("access-control-allow-methods", "*")
+    |> send_resp(200, "{ \"message\": \"ok\" }")
+  end
+
   # Run `docker-compose restart dispatcher` after updating
   # this file.
+
+  ###############
+  # STATIC
+  ###############
+
+  # self-service
+  match "/index.html", %{layer: :static} do
+    forward(conn, [], "http://frontend/index.html")
+  end
+
+  get "/assets/*path", %{layer: :static} do
+    forward(conn, path, "http://frontend/assets/")
+  end
+
+  get "/@appuniversum/*path", %{layer: :static} do
+    forward(conn, path, "http://frontend/@appuniversum/")
+  end
+
+  #################
+  # FRONTEND PAGES
+  #################
+
+  # self-service
+  match "/*path", %{layer: :fall_back, accept: %{html: true}} do
+    # we don't forward the path, because the app should take care of this in the browser.
+    forward(conn, [], "http://frontend/index.html")
+  end
 
   #################################################################
   # Login logic
